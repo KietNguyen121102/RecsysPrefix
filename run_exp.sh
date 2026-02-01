df1c48fbe507c1169f4b008ce25199083665d583
Rebecca <rsalgani@node2x64a.cs.rochester.edu>
2026-02-01 09:09:03 -0500

    bash scripts

--- run_exp.sh
+++ run_exp.sh
@@ -1,19 +1,52 @@
 
+#!/usr/bin/env bash
+
+INPUT="/u/rsalgani/2024-2025/RecsysPrefix/data/ml-1m/full_recset.csv" #"/u/rsalgani/2024-2025/RecsysPrefix/data/ml-1m/full_recset.csv"
+OUTDIR="/data2/rsalgani/Prefix/ml-1m/agg_files" #"/data2/rsalgani/Prefix/ml-1m/agg_files"
+GROUP_FILE="/u/rsalgani/2024-2025/RecsysPrefix/data/ml-1m/item_groups.pkl" #"/u/rsalgani/2024-2025/RecsysPrefix/data/ml-1m/item_groups.pkl"
+
+PYTHON="/data2/rsalgani/miniconda3/envs/prefix/bin/python"
+SCRIPT="/u/rsalgani/2024-2025/RecsysPrefix/02b_generate_agg_with_sampling.py"
+
+NSAMPLES=1
+USERS=10
+ITEMS=30
+
+mkdir -p "${OUTDIR}/logs"
+
+echo "[Launch] aggregation"
+echo "PYTHON=${PYTHON}"
+echo "SCRIPT=${SCRIPT}"
+echo "OUTDIR=${OUTDIR}"
+
 #Run aggregation methods 
-python -u /u/rsalgani/2024-2025/RecsysPrefix/02_generate_agg_with_sampling.py \
-    --input /u/rsalgani/2024-2025/RecsysPrefix/data/ml-1m/recommendations.csv \
-    --outdir /data2/rsalgani/Prefix/ml-1m/agg_files \
-    --n-samples 100 \
-    --user-sample-size 10 \
-    --item-sample-size 30 \
-    --group-file data/ml-1m/item_groups.pkl
+"${PYTHON}" -u "${SCRIPT}" \
+  --input "${INPUT}" \
+  --outdir "${OUTDIR}" \
+  --n-samples "${NSAMPLES}" \
+  --user-sample-size "${USERS}" \
+  --item-sample-size "${ITEMS}" \
+  --group-file "${GROUP_FILE}"
+
+
+
+
+
+
+# python -u /u/rsalgani/2024-2025/RecsysPrefix/02_generate_agg_with_sampling.py \
+#     --input /u/rsalgani/2024-2025/RecsysPrefix/data/ml-1m/recommendations.csv \
+#     --outdir /data2/rsalgani/Prefix/ml-1m/agg_files \
+#     --n-samples 1 \
+#     --user-sample-size 10 \
+#     --item-sample-size 30 \
+#     --group-file data/ml-1m/item_groups.pkl
 
 ##Run Axiom Satisfaction Calculation
-for k in $(seq 0 1); do
-    python -u /u/rsalgani/2024-2025/RecsysPrefix/03b_axiom_satisfaction_calc.py \
-        --agg /data2/rsalgani/Prefix/ml-1m/agg_files/sample_${k} \
-        --pref /data2/rsalgani/Prefix/ml-1m/agg_files/sample_${k}/sampled_rankings.pkl     
-done 
+# for k in $(seq 0 1); do
+#     python -u /u/rsalgani/2024-2025/RecsysPrefix/03b_axiom_satisfaction_calc.py \
+#         --agg /data2/rsalgani/Prefix/ml-1m/agg_files/sample_${k} \
+#         --pref /data2/rsalgani/Prefix/ml-1m/agg_files/sample_${k}/sampled_rankings.pkl     
+# done 
 
 # ##Calculate diversity metrics for each sample
 # for k in $(seq 0 99); do
--- run_parallel_exp.sh
+++ run_parallel_exp.sh
@@ -1,56 +1,50 @@
 #!/usr/bin/env bash
-
-#Run aggregation methods 
 set -euo pipefail
 
-PYTHON="/data2/rsalgani/miniconda3/envs/prefix/bin/python"
-SCRIPT="/u/rsalgani/2024-2025/RecsysPrefix/02_generate_agg_with_sampling_parallel.py"
+# =============================================================================
+# RUNNING AGGREGATION
+# =============================================================================
 
-INPUT="/u/rsalgani/2024-2025/RecsysPrefix/data/ml-1m/full_recset.csv"
-OUTDIR="/data2/rsalgani/Prefix/ml-1m/agg_files"
-GROUP_FILE="/u/rsalgani/2024-2025/RecsysPrefix/data/ml-1m/item_groups.pkl"
 
-NSAMPLES=100
-USERS=10
-ITEMS=30
 
-# parallelism inside each sample
+# PYTHON="/data2/rsalgani/miniconda3/envs/prefix/bin/python"
+# SCRIPT="/u/rsalgani/2024-2025/RecsysPrefix/02b_generate_agg_with_sampling_parallel.py"
+
+# NSAMPLES=100
+# USERS=10
+# ITEMS=30
 # JOBS=4   # adjust based on CPU + SCIP stability (try 4/8/16/24)
 
 # echo "[Launch] Parallel aggregation"
 # echo "PYTHON=${PYTHON}"
 # echo "SCRIPT=${SCRIPT}"
-# echo "OUTDIR=${OUTDIR}"
 # echo "NSAMPLES=${NSAMPLES} USERS=${USERS} ITEMS=${ITEMS} JOBS=${JOBS}"
-# mkdir -p "${OUTDIR}/logs"
+# # mkdir -p "${OUTDIR}/logs"
 # "${PYTHON}" -u "${SCRIPT}" \
-#   --input "${INPUT}" \
-#   --outdir "${OUTDIR}" \
+#   --dataset "goodreads" \
 #   --n-samples "${NSAMPLES}" \
 #   --user-sample-size "${USERS}" \
 #   --item-sample-size "${ITEMS}" \
-#   --group-file "${GROUP_FILE}" \
 #   --jobs "${JOBS}" 
 
 
-
-# set -euo pipefail
+# =============================================================================
+# RUNNING AXIOM SATISFACTION CALCULATION
+# =============================================================================
 
 PYTHON="/data2/rsalgani/miniconda3/envs/prefix/bin/python"
 SCRIPT="/u/rsalgani/2024-2025/RecsysPrefix/03b_axiom_satisfaction_calc_parallel.py"
-BASE="/data2/rsalgani/Prefix/ml-1m/agg_files"
+BASE="/data2/rsalgani/Prefix/goodreads/agg_files" #"/data2/rsalgani/Prefix/ml-1m/agg_files"
 
 MAX_JOBS=8 #was 8 
 WORKERS=24
-
-mkdir -p "${BASE}/logs"
-
 running=0
 
 for k in $(seq 0 99); do
   echo "[Dispatch] sample_${k}"
 
   "${PYTHON}" -u "${SCRIPT}" \
+    --dataset "goodreads" \
     --agg "${BASE}/sample_${k}" \
     --pref "${BASE}/sample_${k}/sampled_rankings.pkl" \
     --workers "${WORKERS}" \
