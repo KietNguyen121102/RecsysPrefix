13220ff892a61e8e5d06823383bf535987c2a6e4
Rebecca <rsalgani@node2x64a.cs.rochester.edu>
2026-02-01 15:54:08 -0500

    parallel script update

--- run_parallel_exp.sh
+++ run_parallel_exp.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+DATASET="$2"
+echo "Dataset: ${DATASET}"
 # =============================================================================
 # RUNNING AGGREGATION
 # =============================================================================
-
-
 
 # PYTHON="/data2/rsalgani/miniconda3/envs/prefix/bin/python"
 # SCRIPT="/u/rsalgani/2024-2025/RecsysPrefix/02b_generate_agg_with_sampling_parallel.py"
@@ -19,9 +19,9 @@ set -euo pipefail
 # echo "PYTHON=${PYTHON}"
 # echo "SCRIPT=${SCRIPT}"
 # echo "NSAMPLES=${NSAMPLES} USERS=${USERS} ITEMS=${ITEMS} JOBS=${JOBS}"
-# # mkdir -p "${OUTDIR}/logs"
+
 # "${PYTHON}" -u "${SCRIPT}" \
-#   --dataset "goodreads" \
+#   --dataset "${DATASET}" \
 #   --n-samples "${NSAMPLES}" \
 #   --user-sample-size "${USERS}" \
 #   --item-sample-size "${ITEMS}" \
@@ -32,23 +32,58 @@ set -euo pipefail
 # RUNNING AXIOM SATISFACTION CALCULATION
 # =============================================================================
 
-PYTHON="/data2/rsalgani/miniconda3/envs/prefix/bin/python"
-SCRIPT="/u/rsalgani/2024-2025/RecsysPrefix/03b_axiom_satisfaction_calc_parallel.py"
-BASE="/data2/rsalgani/Prefix/goodreads/agg_files" #"/data2/rsalgani/Prefix/ml-1m/agg_files"
+# PYTHON="/data2/rsalgani/miniconda3/envs/prefix/bin/python"
+# SCRIPT="/u/rsalgani/2024-2025/RecsysPrefix/03b_axiom_satisfaction_calc_parallel.py"
+# BASE="/data2/rsalgani/Prefix/${DATASET}/agg_files" #"/data2/rsalgani/Prefix/ml-1m/agg_files"
 
-MAX_JOBS=8 #was 8 
-WORKERS=24
+# MAX_JOBS=8 #was 8 
+# WORKERS=24
+# running=0
+
+# mkdir -p "${BASE}/axiom_logs"
+
+# for k in $(seq 0 99); do
+#   echo "[Dispatch] sample_${k}"
+
+#   "${PYTHON}" -u "${SCRIPT}" \
+#     --dataset "${DATASET}" \
+#     --agg "${BASE}/sample_${k}" \
+#     --pref "${BASE}/sample_${k}/sampled_rankings.pkl" \
+#     --workers "${WORKERS}" \
+#     > "${BASE}/axiom_logs/sample_${k}.out" 2> "${BASE}/axiom_logs/sample_${k}.err" &
+
+#   running=$((running + 1))
+
+#   if [[ "${running}" -ge "${MAX_JOBS}" ]]; then
+#     wait -n
+#     running=$((running - 1))
+#   fi
+# done
+
+# wait
+# echo "All samples finished."
+
+# =============================================================================
+# RUNNING KENDALL TAU CALCULATION
+# =============================================================================
+
+PYTHON="/data2/rsalgani/miniconda3/envs/prefix/bin/python"
+SCRIPT="/u/rsalgani/2024-2025/RecsysPrefix/03a_kendall_tau_calc_parallel.py"
+BASE="/data2/rsalgani/Prefix/${DATASET}/agg_files" 
+
+MAX_JOBS=8
 running=0
+
+mkdir -p "${BASE}/kt_logs"
 
 for k in $(seq 0 99); do
   echo "[Dispatch] sample_${k}"
 
   "${PYTHON}" -u "${SCRIPT}" \
-    --dataset "goodreads" \
+    --dataset "${DATASET}" \
     --agg "${BASE}/sample_${k}" \
     --pref "${BASE}/sample_${k}/sampled_rankings.pkl" \
-    --workers "${WORKERS}" \
-    > "${BASE}/logs/sample_${k}.out" 2> "${BASE}/logs/sample_${k}.err" &
+    > "${BASE}/kt_logs/sample_${k}.out" 2> "${BASE}/kt_logs/sample_${k}.err" &
 
   running=$((running + 1))
 
@@ -60,3 +95,42 @@ done
 
 wait
 echo "All samples finished."
+
+
+# =============================================================================
+# RUNNING DIVERSITY CALCULATION
+# =============================================================================
+
+PYTHON="/data2/rsalgani/miniconda3/envs/prefix/bin/python"
+SCRIPT="/u/rsalgani/2024-2025/RecsysPrefix/03c_calc_diversity.py"
+BASE="/data2/rsalgani/Prefix/${DATASET}/agg_files" 
+
+MAX_JOBS=8
+running=0
+
+mkdir -p "${BASE}/div_logs"
+
+for k in $(seq 0 99); do
+  echo "[Dispatch] sample_${k}"
+
+  "${PYTHON}" -u "${SCRIPT}" \
+    --dataset "${DATASET}" \
+    --agg "${BASE}/sample_${k}" \
+    --pref "${BASE}/sample_${k}/sampled_rankings.pkl" \
+    > "${BASE}/div_logs/sample_${k}.out" 2> "${BASE}/div_logs/sample_${k}.err" &
+
+  running=$((running + 1))
+
+  if [[ "${running}" -ge "${MAX_JOBS}" ]]; then
+    wait -n
+    running=$((running - 1))
+  fi
+done
+
+wait
+echo "All samples finished."
+
+
+python3 /u/rsalgani/2024-2025/RecsysPrefix/04_gather_results.py --dataset "${DATASET}"
+
+echo "All done."
